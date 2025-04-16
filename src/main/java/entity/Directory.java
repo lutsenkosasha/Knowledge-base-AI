8a3b5147bd93953d0cb89fe5b19b326c5ccd2133
ЕкатеринаПанина <katerina_panina1@mail.ru>
2025-04-16 22:42:18 +0300

    Add entities classes

--- src/main/java/entity/Directory.java
+++ src/main/java/entity/Directory.java
@@ -1,4 +1,44 @@
 package entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+import java.util.List;
+
+@Entity
+@Table(name = "directory")
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Directory extends Auditable {
+    @Id
+    @Column(name = "directory_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long directoryId;
+
+    @Column(name = "count_files")
+    @NotNull
+    @Min(value = 0)
+    private Integer countFiles;
+
+    @Column(name = "department")
+    @NotNull
+    private String department;
+
+    @Column(name = "directory_name")
+    @NotNull
+    private String directoryName;
+
+    @Column(name = "directory_size")
+    @Min(value = 0)
+    private Long directorySize;
+
+    @OneToMany(mappedBy = "directory")
+    private List<File> files;
 }
