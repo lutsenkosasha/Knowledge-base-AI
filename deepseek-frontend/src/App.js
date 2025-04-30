0869faf77f65c223b605074cfc88fb0a33a70272
kulishkinilya <kulishkinilya@yandex.ru>
2025-04-30 14:12:17 +0300

    Сделал чат с deepseek через веб-интерфейс, пофиксил некоторые проблемы

---
+++ deepseek-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Chat from './components/Chat';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>KnowledgeBaseAI</h1>
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
