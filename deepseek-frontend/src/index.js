2ac0705bcbefc20b7dc41f6d8541f8b92bdf6102
ЕкатеринаПанина <katerina_panina1@mail.ru>
2025-05-02 12:20:39 +0300

    create pages for all directories (view all and save new) and for current directory

--- deepseek-frontend/src/index.js
+++ deepseek-frontend/src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
 import App from './App';
+import AllDirectories from './components/AllDirectories';
+import Directory from './components/Directory';
 import reportWebVitals from './reportWebVitals';
+
+const router = createBrowserRouter([
+  {
+    path: "/chat/:id",
+    element: <App />,
+  },
+  {
+    path:"/",
+    element: <AllDirectories />
+  },
+  {
+    path:"/directories/:id",
+    element: <Directory />,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
